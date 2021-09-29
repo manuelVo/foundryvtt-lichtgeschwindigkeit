d2a28c000994e5654f93804c1122001c65cc1aeb
Manuel Vögele <develop@manuel-voegele.de>
2021-09-29 08:30:50 +0200

    Don't overcull inverted walls when sight angle is limited (fixes #32)

--- rust/src/raycasting/vision_angle.rs
+++ rust/src/raycasting/vision_angle.rs
@@ -22,7 +22,7 @@ pub fn restrict_vision_angle(
 				}
 				wall_inverted = false;
 			} else {
-				if end.borrow().angle <= vision_angle.start {
+				if end.borrow().angle <= vision_angle.start && start.borrow().angle >= vision_angle.end {
 					return Some([None, None]);
 				}
 				wall_inverted = true;
